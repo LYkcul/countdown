1062afb2f2a64acfc0e1e62ef9e99bba89cfd5dc
LYkcul <liu.qdcy@foxmail.com>
2023-03-07 19:06:28 +0800

    first commit

---
+++ handler.ts
@@ -0,0 +1,9 @@
+import { HomeHandler } from 'hydrooj/src/handler/home'
+import moment from 'moment';
+
+async function getCountdown() {
+
+}
+HomeHandler.prototype.getCountdown = async (domainId) => {
+    return await getCountdown();
+}
